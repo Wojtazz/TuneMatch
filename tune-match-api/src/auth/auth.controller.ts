6cca532a7ec6564539aea1af47639f6fe6ca2b42
Wojciech Rachwał <w_rachwal@inf15.local>
2025-01-08 10:50:13 +0100

    authentication start

---
+++ tune-match-api/src/auth/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Profile } from 'passport-spotify';
+import { Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(SpotifyOauthGuard)
+  @Get('login')
+  login() {
+    return;
+  }
+
+  @UseGuards(SpotifyOauthGuard)
+  @Get('redirect')
+  async spotifyAuthRedirect(
+    @Req() req: any,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const {
+      user,
+      authInfo,
+    }: {
+      user: Profile;
+      authInfo: {
+        accessToken: string;
+        refreshToken: string;
+        expires_in: number;
+      };
+    } = req;
+
+    if (!user) {
+      res.redirect('/');
+      return;
+    }
+
+    req.user = undefined;
+
+    const jwt = this.authService.login(user);
+
+    res.set('authorization', `Bearer ${jwt}`);
+
+    return res.status(201).json({ authInfo, user });
+  }
+}
