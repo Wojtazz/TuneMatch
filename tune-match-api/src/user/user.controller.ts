ea0e0743b40831bba72b101fe76de34c3ba0cb29
Wojciech Rachwal <wojciech.rachwal@kolomolo.com>
2025-06-17 15:44:49 +0200

    feat: user controller update

--- tune-match-api/src/user/user.controller.ts
+++ tune-match-api/src/user/user.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.schema';
 import { TicketmasterService } from 'src/ticketmaster/ticketmaster.service';
+import { Concert } from 'src/ticketmaster/ticketmaster.interface';
 
 @Controller('user')
 export class UserController {
@@ -13,9 +14,32 @@ export class UserController {
     @InjectModel('User') private readonly userModel: Model<User>,
   ) {}
 
-  @Get()
-  async test() {
-    const users = await this.userModel.find().exec();
-    await this.userService.matchUser(users[0]);
+  @Get('/match/:id')
+  async matchUser(@Param('id') id: string): Promise<void> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return await this.userService.matchUser(user);
+  }
+
+  @Get('/update-user-concerts/:id')
+  async updateUserConcerts(@Param('id') id: string): Promise<Concert[]> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    const concerts = await this.userService.getConcertsForUser(user);
+    if (concerts.length > 0) {
+      await this.userService.updateUser(user._id as unknown as string, {
+        $push: { proposedConcerts: { $each: concerts } },
+      });
+    }
+
+    return concerts;
   }
 }
